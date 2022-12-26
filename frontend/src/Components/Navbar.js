38d461f000fa0b66d045471aed5d33e624e99668
mariyabaig <mariya.baig@nagarro.com>
2022-12-27 02:58:35 +0530

    Navbar fixed

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -3,14 +3,22 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-        <nav className="navbar navbar-light bg-light justify-content-between">
-  <a className="navbar-brand">Navbar</a>
+    <div className='flex flex-col justify-evenly'>
+       <nav className="navbar navbar-dark bg-dark">
+  <a className="navbar-brand mx-3">Valuepitch</a>
+
   <form className="form-inline">
-    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+  <div className='flex flex-col justify-end items-center mx-3'>
+  <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </div>
+   
   </form>
 </nav>
+</div>
     </>
+    
+       
   )
 }
 
