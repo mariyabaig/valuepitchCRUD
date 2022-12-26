018ac9a37e6928a6dbce13c71e280b8658a96fdc
mariyabaig <mariya.baig@nagarro.com>
2022-12-27 02:15:02 +0530

    Getting data at front-end

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -4,8 +4,12 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import AddIcon from '@mui/icons-material/Add';
+import { useEffect,useState } from "react";
+
 
 const Home = () => {
+
+//useNavigate hook to navigate to another page
 const navigate = useNavigate();
 
 const goToregister = ()=>{
@@ -20,21 +24,44 @@ const goToEdit = ()=>{
     navigate("/details")
     }
 
+//get data on home page
+const [getUserdata, setUserdata] = useState([]);
+console.log(getUserdata);
+
+const getData = async(e) => { 
+  const res = await fetch("/getdata", {
+      method: "GET",
+      headers: {
+          "Content-Type": "application/json"
+      }
+  });
+
+  const data = await res.json();
+  console.log(data);
+
+  if (res.status === 422 || !data) {
+      console.log("error");
+
+  } else {
+      setUserdata(data)
+      console.log("Data received through get api");
+
+  }
+}
+
+useEffect(() => {
+  getData();
+}, [])
+
 
   return (
     <div>
     <div className="container flex flex-col">
-      {/* <div className="flex flex-colitems-end">  <button
-          onClick={console.log("clicked")}
-          className="mt-3 btn btn-primary text-white py-2 px-2"
-        >
-          Add data
-        </button></div> */}
       
-      
-      <table className="table table-dark table-striped my-3">
+ <table className="table table-dark table-striped my-3">
         <thead>
           <tr>
+          <th scope="col">ID</th>
             <th scope="col">Name</th>
             <th scope="col">Email</th>
             <th scope="col">DOB</th>
@@ -44,7 +71,32 @@ const goToEdit = ()=>{
           </tr>
         </thead>
         <tbody>
-          <tr>
+        {
+                                getUserdata.map((element, id) => {
+                                    return (
+                                        <>
+                                            <tr>
+                                                <th scope="row">{id + 1}</th>
+                                                <td>{element.name}</td>
+                                                <td>{element.email}</td>
+                                                <td>{element.dob}</td>
+                                                <td>{element.address}</td>
+                                                <td>{element.country}</td>
+                                                <td className="d-flex justify-content-between">
+                                                <button onClick={goToDetails} className="btn btn-primary mx-3"><RemoveRedEyeIcon/></button>
+                                                <button onClick={goToEdit} className="btn btn-success mx-3"><EditIcon/></button>
+                                                <button className="btn btn-danger mx-3"><DeleteIcon/></button>
+                                                </td>
+                                            </tr>
+                                        </>
+                                    )
+                                })
+                            }
+
+
+
+        </tbody>
+          {/* <tr>
             <th scope="row">1</th>
             <td>Mark</td>
             <td>Otto</td>
@@ -81,7 +133,7 @@ const goToEdit = ()=>{
             </td>
             
           </tr>
-        </tbody>
+        </tbody> */}
       </table>
       <div className="flex flex-col items-end">  <button className="btn btn-success pt-2 py-2" onClick = {goToregister}>Add new data <AddIcon/></button></div>
     </div>
